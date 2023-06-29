fc3237397f6f701ba8bb375f25dd62b4db703874
Liz Hernandez <lizcarol2005@gmail.com>
2023-06-29 11:27:35 -0400

    Initial Commit

---
+++ FinalProject_ARL/FinalProject_ARLApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject_ARLApp.swift
+//  FinalProject_ARL
+//
+//  Created by Tomas Hernandez on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProject_ARLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
